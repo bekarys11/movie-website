c44885f16cd24aee08e286051c42025908dcea93
bekarys11 <ibekarys@gmail.com>
2020-10-01 20:18:15 +0600

    hid some sensitive info

--- src/components/API.js
+++ src/components/API.js
@@ -1,6 +1,6 @@
-export const FEATURED_API =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
-export const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+const mykey = config.KEY;
+
+export const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${mykey}&page=1`;
+export const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${mykey}&query=`;
 
 export const IMG_API = 'https://image.tmdb.org/t/p/w1280';
